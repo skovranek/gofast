63be5cacf9bda07d5e7b0706e1866d5861ce195c
skovranek <59619403+skovranek@users.noreply.github.com>
2023-10-18 03:17:56 -0400

    add 'chdir' to main.py, add 'go mod init' cmd to main.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import subprocess
 import sys
 from ruamel.yaml import YAML
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate an API and/or static server in Go according to an OpenAPI spec in a YAML file.")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verbose", action="store_true")
-    group.add_argument("-q", "--quiet", action="store_true")
-    parser.add_argument("yaml_file", help="The YAML file containing the OpenAPI spec.")
-    parser.add_argument("destination", help="The location of the directory in which to generate your Go module.")
+    parser = argparse.ArgumentParser(
+        prog='GoFast',
+        description="Generate an API and/or static server in Go according to an OpenAPI spec in a YAML file.",
+        epilog='Created by Matt Skovranek'
+    )
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="display detailed processing information"
+    )
+    verbosity.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="display minimal information"
+    )
+    parser.add_argument(
+        "yaml_file",
+        help="the YAML file containing the OpenAPI spec",
+    )
+    parser.add_argument(
+        "destination",
+        help="the location of the directory in which to generate your Go module",
+    )
     args = parser.parse_args()
 
     with open(args.yaml_file, 'r') as yaml_data:
@@ -23,10 +44,18 @@ def main():
 
     dir = args.destination
 
-    #subprocess.run(f"mkdir {dir}", shell=True)
+    os.chdir(dir)
+    subprocess.run(f"go mod init {data['path']}", shell=True)
 
-    with open(f'{dir}/output.txt', 'a') as f:
+    with open(f'output.txt', 'a') as f:
         f.write(data['key'])
+
+    if args.quiet:
+        print("done")
+    elif args.verbose:
+        print(f"Writing '{data['key']}' to '{dir}/output.txt'")
+    else:
+        print(f"Writing to '{dir}/output.txt'")
 
 if __name__ == "__main__":
     main()
